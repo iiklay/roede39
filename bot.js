10ca18740fec61bd396c18ce557698e59751e4cd
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-23 21:34:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2429,28 +2429,6 @@ client.on("message", (message) => {
         if (!channel) return message.reply('**There is no room like this name -_-**').catch(console.error);
         channel.delete()
     }
-});  
-client.on("message", (message) => {
-  var sender = message.author
-if(message.content.startsWith(prefix + 'daily')) {
-if (games[sender.id].lastDaily != moment().format('day')) {
-    games[sender.id].lastDaily = moment().format('day')
- games[message.author.id].credits += 200;
-    message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
-} else {
-    message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
-}
-}
-})
-//مصاري//
-client.on("message", (message) => {
-  if (message.author.bot) return;
-    if (message.author.id === client.user.id) return;
-	if(!message.channel.guild) return;       
-if (message.content === '-credits') {
-message.channel.send(`** ${message.author.username}, your :credit_card: balance is ${games[message.author.id].credits}.**`)
-}
-});
 
 
 // THIS  MUST  BE  THIS  WAY

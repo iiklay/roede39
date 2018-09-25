0b077582c07a8aa9389ee196c9012abfd9933d7c
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-25 14:51:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3036,9 +3036,9 @@ client.on('message', message => {
   }
  
  
-  if (message.content.startsWith('+bc')) {
+  if (message.content.startsWith('-brod')) {
           if (!args[0]) {
-message.channel.send("**+bc <message>**");
+message.channel.send("```**-bc <message>**```");
 return;
 }
 message.guild.members.forEach(m => {

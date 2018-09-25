5578701985681b247fb5b87bd25e6ea6a502e0b2
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-25 14:45:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3027,5 +3027,27 @@ m.sendMessage(args)
 })
 }
 });		
+client.on('message', message => {
+   
+    let args = message.content.split(' ').slice(1).join(' ');
+   
+  if (message.content === 'ping') {
+      message.channel.send(`<@${message.author.id}> Ping..!`)
+  }
+ 
+ 
+  if (message.content.startsWith('+bc')) {
+          if (!args[0]) {
+message.channel.send("**+bc <message>**");
+return;
+}
+message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+   m.send(`${args}`);
+ 
+});
+  }
+ 
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

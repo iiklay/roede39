50e110cace630d3f4b2e297de12d7ba2e984237c
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-26 10:19:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2999,22 +2999,5 @@ client.on('message', msg => {
     }
 }
 });
-client.on('message', message => {
-  let log = message.guild.channels.find('name', "log") 
-  let act = message.guild.roles.find('name', "-new")
-  let user = message.mentions.members.first();
-  if(message.content.startsWith(prefix + "act")){
-    var embed = new Discord.RichEmbed() 
-    .setAuthor(message.author.username) 
-    .setThumbnail(user.avatarURL)
-    .addField('User Activated', ${user} get rank ${act})
-    .addField('By', <@${message.author.id}>)
-    .setTimestamp()
-    .setFooter("Codescopyright")
-  log.send({embed})
-  message.channel.send({embed})
-  user.addRole(${act})
-  }
-});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

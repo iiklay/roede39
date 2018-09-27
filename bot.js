18bf969da90261679ffd7fac57244b9e6b2e44aa
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-27 09:13:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3029,5 +3029,23 @@ client.on('message', message => {
   message.channel.send(embed);
 }
 });
+  client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 2,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
+
+message.author.send(`**مدة الرابط : يـوم
+عدد استخدامات الرابط : 2**`)
+
+
+    }
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

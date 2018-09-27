1b081415b012a9add8026f71cf23feb34d850e0f
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-27 15:52:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1435,27 +1435,6 @@ msg.channel.send(embed).then(() => {
     spee[id] = (new Date).getTime()
 }
 });
-
-
-client.on('ready', function(){	
-    var ms = 40000 ;	
-    var setGame = ['-inv','-help'];	
-    var i = -1;	
-    var j = 0;	
-    setInterval(function (){	
-        if( i == -1 ){	
-j = 1;	
-       }	
-        if( i == (setGame.length)-1 ){	
-            j = -1;	
-      }	
-       i = i+j;	
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
-}, ms);	
-	
-});
-
-
 client.on('message', message => {
   var prefix ="-"; 
 if (message.content.startsWith(prefix + 'perms')) {

76c09a77594eb2835cfbb9df1afe69ab51c4e571
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-29 08:41:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2412,7 +2412,7 @@ if (message.content.startsWith("-ct")) {
             if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
         let args = message.content.split(" ").slice(1);
     message.guild.createChannel(args.join(' '), 'text');
-message.channel.sendMessage('تـم إنـشاء روم كـتابـي')
+message.channel.sendMessage('```تـم إنـشاء روم كـتابـي```')
 
 }
 });

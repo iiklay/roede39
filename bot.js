98bf11608587b83b4bdb46e9c75503b70dcd3365
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-29 14:25:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2899,35 +2899,5 @@ client.setInterval(function() {
   }, time);
 }
 });
-client.on('message', message => {
-const yt = require('ytdl-core');
-  if (message.content.startsWith('-قران')) {
-              if(!message.channel.guild) return message.reply('** This command only for servers **');
-
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`من فضلك ادخل روم صوتي `);
-    }
-    voiceChannel.join()
-      .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=9-oGnGaI9Ps&t=8009s', {audioonly: true});
-        const dispatcher = connnection.playStream(stream);
-        dispatcher.on('end', () => {
-          voiceChannel.leave();
-        });
-      });
-  }
-  
-  if (message.content.startsWith('-توقف')) {
-              if(!message.channel.guild) return message.reply('** This command only for servers **');
-
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-      return message.reply(`من فضلك ادخل روم صوتي `);
-    }
-voiceChannel.leave();
-  }
-
-});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

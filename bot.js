4b705f8940cfa6164ca792a92ac98da703c15a9a
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-30 09:49:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-const Discord = require('discord.js');
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(` اكتب اي شي هنا  `,"http://twitch.tv/S-F")
